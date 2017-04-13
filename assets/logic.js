9557c2dc29b961dc184619da908a69e981048d7f
Derek Greene <dg74155@gmail.com>
2017-04-12 23:52:52 -0400

    api and table

---
+++ assets/logic.js
@@ -0,0 +1,35 @@
+
+
+$("#button").on("click", function() {
+	event.preventDefault();
+	$("tbody").empty();
+	$("#table").show();
+	$("#slide").hide();
+	console.log($("#CityInput").val());
+    var oArgs = {
+
+        app_key: "k8DZJrCKTzw5f9QC",
+
+        location: $("#CityInput").val(),
+
+        page_size: 10,
+
+    };
+
+    EVDB.API.call("/events/search", oArgs, function(oData) {
+    	if (!oData.events) {
+    		console.log("no events");
+    	} else {
+	    	for (var i = 0; i < oData.events.event.length; i++) {
+	    		console.log(oData.events.event[i].title);
+	    			var results = oData.events.event[i].title;
+	    		$("tbody").append("<tr><td>" + results+ "</td><td>" + "i" + "</td><td>" + results + "</td><td>" +
+	    			"i" + "</td></tr>");
+	    	}
+	    }
+
+    });
+});
+
+
+
